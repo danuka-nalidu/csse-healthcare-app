15369f5e5b76922d59e112e26b4de71cf61d142d
danuka-nalidu <danukanalidu2002@gmail.com>
2024-10-14 21:17:40 +0530

    Initial commit

---
+++ frontend/src/pages/Doctors/DoctorDetails.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const DoctorDetails = () => {
+  return (
+    <div>DoctorDetails</div>
+  );
+}
+
+export default DoctorDetails;
---
+++ frontend/src/pages/Signup.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Signup = () => {
+  return (
+    <div>SiguUp</div>
+  );
+}
+
+export default Signup; 
