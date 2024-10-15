f0332af78d00c2d032d34a524be4f0dcf7c66f2e
danuka-nalidu <danukanalidu2002@gmail.com>
2024-10-15 13:33:59 +0530

    Add Hero section

--- frontend/src/layout/Layout.jsx
+++ frontend/src/layout/Layout.jsx
@@ -11,7 +11,7 @@ const Layout = () => {
       <main>
         <Routers />
       </main>
-      <Footer /> 
+    <Footer /> 
     </>
   );
 };
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,9 +1,26 @@
 import React from "react";
-import Header from "../components/Header/Header";
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <>
+      {/* Hero section */}
+      <>
+      <section className="hero_section pt-[60px] 2xl:h-[800px]">
+        <div className="container">
+          <div className="flex flex-col lg:flex-row gap-[90px] items-center justify-between">
+            {/* hero content */}
+            <div>
+              <div className="lg:w-[570px]">
+                <h1 className="text-[36px] leading-[46px] text-headingColor font-[800] md:text-[60px] md:leading-[70px]">We help patients live a healthy, longer life</h1>
+                <p className="text_para">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius voluptatem voluptas, amet sit nemo soluta qui nostrum expedita magnam labore tenetur magni deserunt, nihil hic rem impedit ut dolorum numquam.</p>
+                <button className="btn">Request an appointment</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      </>
+    </>
   );
 }
 
